b618639ed3e1d3e51cc0de5b9918034226a6adc3
Nith2005 <vnithin2005@gmail.com>
2025-03-09 21:22:58 +0530

    deleted

--- HandWrittenDigitRecognition.py
+++ HandWrittenDigitRecognition.py
@@ -10,3 +10,29 @@ for ax, image, label in zip(axes, df.images, df.target):
   ax.set_axis_off()
   ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
   ax.set_title("Training: %i" % label)
+
+df.images.shape
+
+df.images[0]
+
+df.images[0].shape
+
+len(df.images)
+
+n_samples = len(df.images)
+data = df.images.reshape((n_samples,-1))
+
+data[0]
+
+data[0].shape
+
+len(df.images)
+
+n_samples = len(df.images)
+data = df.images.reshape((n_samples,-1))
+data[0]
+
+data[0].shape
+
+data.shape
+
